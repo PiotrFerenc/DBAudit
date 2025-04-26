06e80adc5a0f92e9b24646a7a0abb4fab3d04099
Piotr Ferenc <ferenc.piotr@outlook.com>
2025-04-26 11:57:30 +0200

    Add TableAnalyzer class to DBAudit.Analyzer
    Introduced the TableAnalyzer class implementing IRequest with a SqlConnection parameter. This serves as a foundational component for table analysis functionality within the DBAudit.Analyzer namespace.

---
+++ DBAudit.Analyzer/TableAnalyzer.cs
@@ -0,0 +1,10 @@
+﻿using DBAudit.Infrastructure.Common.Command;
+using LanguageExt;
+using LanguageExt.Common;
+using Microsoft.Data.SqlClient;
+
+namespace DBAudit.Analyzer;
+
+public class TableAnalyzer(SqlConnection connection) : IRequest<Either<string, string>>
+{
+}
