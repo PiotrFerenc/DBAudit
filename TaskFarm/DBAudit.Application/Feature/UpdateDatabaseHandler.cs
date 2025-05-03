8b0ca687da78a4a5c9c8f7058127d82874473d75
Piotr Ferenc <ferenc.piotr@outlook.com>
2025-05-03 19:20:13 +0200

    Refactor UpdateDatabaseHandler to use ICommandDispatcher.
    Replaced unused table analysis logic with ICommandDispatcher to streamline command handling. Added logic to send an AnalyzeDatabase command for better integration with the system. Cleaned up commented-out code for improved readability and maintainability.

--- TaskFarm/DBAudit.Application/Feature/UpdateDatabaseHandler.cs
+++ TaskFarm/DBAudit.Application/Feature/UpdateDatabaseHandler.cs
@@ -6,27 +6,23 @@ using DBAudit.Infrastructure.Storage;
 
 namespace DBAudit.Application.Feature;
 
-public class UpdateDatabaseHandler(IDatabaseProvider databaseProvider, ITableService tableService, IQueueProvider queueProvider, IAnalyzerService tableAnalyzerService) : ICommandHandler<DatabaseMessage>
+public class UpdateDatabaseHandler(IDatabaseProvider databaseProvider, ITableService tableService, IQueueProvider queueProvider, ICommandDispatcher dispatcher) : ICommandHandler<DatabaseMessage>
 {
     public async Task HandleAsync(DatabaseMessage message)
     {
         var tables = await databaseProvider.GetTables(message.EnvId, message.DbId);
-        // var cs = databaseProvider.GetConnectionString(message.EnvId, message.DbId);
-        // await cs.IfSomeAsync(async connectionString =>
-        // {
-        //     var connection = new SqlConnection(connectionString);
-        //     var analyzers = tableAnalyzerService.GetDatabaseAnalyzers(connection);
-        //     foreach (var analyzer in analyzers)
-        //     {
-        //         var result = await dispatcher.Send(analyzer);
-        //     }
-        // });
 
         foreach (var table in tables.Where(table => !tableService.Exist(message.DbId, message.EnvId, table.Name)))
         {
             table.DatabaseId = message.DbId;
             table.EnvironmentId = message.EnvId;
             tableService.Add(table);
+            await dispatcher.Send(new AnalyzeDatabase
+            {
+                EnvId = message.EnvId,
+                DbId = message.DbId,
+            });
+
             queueProvider.Enqueue(new ColumnsMessage(message.EnvId, message.DbId, table.Id));
         }
     }
