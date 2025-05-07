f52ccb768a1babcb57fa78ef23314b49260a6dcc
Piotr Ferenc <ferenc.piotr@outlook.com>
2025-05-07 11:44:12 +0200

    Add ReportService implementation and IReportService interface
    Introduced the IReportService interface and its concrete implementation, ReportService. These provide methods to manage reports, including adding, retrieving, and updating report data with support for counters tied to database IDs.

---
+++ src/DBAudit.Infrastructure.Storage.Binary/ReportService.cs
@@ -0,0 +1,13 @@
+using DBAudit.Infrastructure.Contracts.Entities;
+using LanguageExt;
+
+namespace DBAudit.Infrastructure.Storage.Binary;
+
+public class ReportService(IDbAuditStorage<ReportView> storage) : IReportService
+{
+    public List<ReportView> All(Guid dbId) => storage.Where(x => x.DatabaseId == dbId);
+
+    public void Add(ReportView report) => storage.SaveItem(report);
+    public void AddCounter(Guid dbId, (string Title, string Value) counter) => storage.Update(r => r.Counters.Add(counter), x => x.DatabaseId == dbId);
+    public Option<ReportView> Get(Guid dbId) => storage.Find(x => x.DatabaseId == dbId);
+}
---
+++ src/DBAudit.Infrastructure.Storage/IReportService.cs
@@ -0,0 +1,12 @@
+using DBAudit.Infrastructure.Contracts.Entities;
+using LanguageExt;
+
+namespace DBAudit.Infrastructure.Storage;
+
+public interface IReportService
+{
+    List<ReportView> All(Guid dbId);
+    void Add(ReportView report);
+    void AddCounter(Guid dbId, (string Title, string Value) counter);
+    Option<ReportView> Get(Guid dbId);
+}
