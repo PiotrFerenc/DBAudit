1a1a3136746978545803dd44e833517726e95358
Piotr Ferenc <ferenc.piotr@outlook.com>
2025-05-07 11:44:38 +0200

    Add support for ReportView storage and service registration
    Registered `IDbAuditStorage<ReportView>` and `IReportService` in the dependency injection container. This ensures proper handling and storage of `ReportView` objects alongside their associated services.

--- src/DBAudit.Infrastructure.Storage.Binary/StorageExtensions.cs
+++ src/DBAudit.Infrastructure.Storage.Binary/StorageExtensions.cs
@@ -12,10 +12,12 @@ public static class StorageExtensions
         services.AddSingleton<IDbAuditStorage<Database>>(new BinaryStorage<Database>($"{nameof(Database)}.bin"));
         services.AddSingleton<IDbAuditStorage<Table>>(new BinaryStorage<Table>($"{nameof(Table)}.bin"));
         services.AddSingleton<IDbAuditStorage<Column>>(new BinaryStorage<Column>($"{nameof(Column)}.bin"));
+        services.AddSingleton<IDbAuditStorage<ReportView>>(new BinaryStorage<ReportView>($"{nameof(ReportView)}.bin"));
         
         services.AddTransient<IEnvironmentService, EnvironmentService>();
         services.AddTransient<IDatabaseService, DatabaseService>();
         services.AddTransient<ITableService, TableService>();
         services.AddTransient<IColumnService, ColumnService>();
+        services.AddTransient<IReportService, ReportService>();
     }
 }
